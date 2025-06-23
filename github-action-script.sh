c2ef5d07c558f4e6f0fe6e5d141d7bfa7eb7fb7e
Hrithiksomani <37939834+Hrithiksomani@users.noreply.github.com>
2025-06-23 16:23:48 +0700

    Update github-action-script.sh

--- github-action-script.sh
+++ github-action-script.sh
@@ -20,7 +20,7 @@ else
 fi
 
 # Get tomorrow's date in YYYY-MM-DD format
-BOOKING_DATE=$(date -d "tomorrow" +%Y-%m-%d)
+BOOKING_DATE=$(date -d "tomorrow + 1 day" +%Y-%m-%d)
 
 # Set your booker ID and authorization token
 BOOKER_ID="17001de1-fac3-487c-b009-8c4883e15394"
